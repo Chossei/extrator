dd7f859e470202c11a667302274d6be7453c9930
Átila Prudente <150717036+Chossei@users.noreply.github.com>
2025-09-10 16:51:42 -0300

    teste

--- paginas/principal.py
+++ paginas/principal.py
@@ -3,14 +3,16 @@ from paginas.funcoes import adicionar_variavel
 
 st.title('Extrator de Dados')
 st.markdown('Selecione as variáveis de interesse e extraia os respectivos dados do seu arquivo PDF!')
- 
+st.divider() 
 
 if 'lista_de_variaveis' not in st.session_state:
-        st.session_state.lista_de_variaveis = []
-        st.subheader("Variáveis configuradas:")
-        st.table(st.session_state.lista_de_variaveis)
-else:
-        st.info("Nenhuma variável foi adicionada ainda.")
+    st.session_state.lista_de_variaveis = []
 
-if st.button('Clique aqui'):
+st.header('Variáveis Atualmente Configuradas para Busca')
+if st.session_state.lista_de_variaveis:
+    st.table(st.session_state.lista_de_variaveis)
+else:
+    st.info('Nenhuma variável foi adicionada ainda.')
+
+if st.button('Adicionar variáveis'):
     adicionar_variavel()
