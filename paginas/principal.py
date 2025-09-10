59df4545792529542f194a6476a1e12078e99788
Átila Prudente <150717036+Chossei@users.noreply.github.com>
2025-09-10 15:59:38 -0300

    teste

--- paginas/principal.py
+++ paginas/principal.py
@@ -1,6 +1,9 @@
 import streamlit as st
-
+from paginas.funcoes import adicionar_variavel
 
 st.title('Extrator de Dados')
 st.markdown('Selecione as variáveis de interesse e extraia os respectivos dados do seu arquivo PDF!')
  
+
+if st.button('Clique aqui'):
+    adicionar_variavel()
