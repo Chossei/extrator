6a919872fb9443de10ae8cfe54cec486b70974fd
Átila Prudente <150717036+Chossei@users.noreply.github.com>
2025-10-10 16:39:29 -0300

    atualização

--- paginas/principal.py
+++ paginas/principal.py
@@ -3,8 +3,7 @@ import numpy as np
 import pandas as pd
 from paginas.funcoes import (
     adicionar_variavel,
-    extrator_texto,
-    estruturador
+    estruturador_atualizado
 )
 
 from paginas.funcoes_firebase import (
